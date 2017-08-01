d065277f23c50506aeb5333c720ebff07fec49de
yapalgit <williamvignoles@gmail.com>
2017-08-01 09:57:19 +0200

    Création de toutes les classes DAL et DALTEST

---
+++ src/fr/eni/yapalQCM/dal/ReponseDAL.java
@@ -0,0 +1,91 @@
+/**
+ * @author wvignoles2017
+ * @date 1 août 2017
+ * @version yapalqcm V1.0
+ */
+package fr.eni.yapalQCM.dal;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import fr.eni.yapalQCM.bo.Reponse;
+
+/**
+ * @author wvignoles2017
+ * @date 1 août 2017
+ * @version yapalqcm V1.0
+ */
+public class ReponseDAL implements IDAL<Reponse> {
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.IDAL#getLength()
+	 */
+	@Override
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.IDAL#getOne(int)
+	 */
+	@Override
+	public Reponse getOne(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.IDAL#getAll()
+	 */
+	@Override
+	public List<Reponse> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.IDAL#add(java.lang.Object)
+	 */
+	@Override
+	public boolean add(Reponse t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.IDAL#update(java.lang.Object)
+	 */
+	@Override
+	public boolean update(Reponse t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.IDAL#delete(int)
+	 */
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.IDAL#itemBuilder(java.sql.ResultSet)
+	 */
+	@Override
+	public Reponse itemBuilder(ResultSet rs) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src_tests/fr/eni/yapalQCM/dal/ReponseDALTEST.java
@@ -0,0 +1,75 @@
+/**
+ * @author wvignoles2017
+ * @date 1 août 2017
+ * @version yapalqcm V1.0
+ */
+package fr.eni.yapalQCM.dal;
+
+/**
+ * @author wvignoles2017
+ * @date 1 août 2017
+ * @version yapalqcm V1.0
+ */
+public class ReponseDALTEST implements ITEST {
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.ITEST#testGetLength()
+	 */
+	@Override
+	public void testGetLength() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.ITEST#testGetOne()
+	 */
+	@Override
+	public void testGetOne() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.ITEST#testGetAll()
+	 */
+	@Override
+	public void testGetAll() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.ITEST#testAdd()
+	 */
+	@Override
+	public void testAdd() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.ITEST#testUpdate()
+	 */
+	@Override
+	public void testUpdate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * {@inheritDoc}
+	 * @see fr.eni.yapalQCM.dal.ITEST#testDelete()
+	 */
+	@Override
+	public void testDelete() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
