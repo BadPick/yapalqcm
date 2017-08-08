7cd410a1e587f8b0f6f0e1246844f07bd3dbc56e
YopCoo <yopcoo@gmail.com>
2017-08-08 16:57:36 +0200

    Commit TestsManager

---
+++ src/fr/eni/yapalQCM/bll/TestsManager.java
@@ -0,0 +1,35 @@
+package fr.eni.yapalQCM.bll;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.yapalQCM.bo.Test;
+import fr.eni.yapalQCM.dal.TestDAL;
+
+public class TestsManager {
+	
+	/**
+	 * Classe permettant de récupérer l'ensemble des tests (sans la liste
+	 * de questions et de réponses!!!).
+	 * @return
+	 * @throws SQLException 
+	 */
+	public static List<Test> getTests() throws SQLException{
+		List<Test> liste = new ArrayList<Test>();
+		
+			TestDAL testDal = new TestDAL();
+			liste=testDal.getTests();
+		
+		return liste;
+	}
+
+	public static Test getTest(int idTest) throws SQLException {
+		Test test = null;
+		
+		TestDAL testDal = new TestDAL();
+		test=testDal.getOne(idTest);
+		
+		return test;
+	}
+}
